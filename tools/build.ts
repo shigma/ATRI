4bfa4cb9f75ec3c40729431e85b8a869a8a6cc10
Shigma <1700011071@pku.edu.cn>
2020-08-12 14:15:20 +0800

    spinner

--- tools/build.ts
+++ tools/build.ts
@@ -1,50 +1,65 @@
-import { exec as _exec } from "child_process"
-import glob from "fast-glob"
+import * as cp from "child_process"
 import * as path from "path"
 import * as util from "util"
-import * as assert from "assert"
 import * as fs from "fs"
-
-const exec = util.promisify(_exec);
+import glob from "fast-glob"
+import ora from "ora"
+import ms from "ms"
 
 async function build(workDir: string, filename: string, platform: "win") {
   const outDir = workDir // path.join(workDir, "build"); mkdirSync(outDir)
+  const spinner = ora()
+
+  const _exec = util.promisify(cp.exec)
+  async function exec (command: string, options: cp.ExecOptions = {}) {
+    const timestamp = Date.now()
+    spinner.start(command)
+    const { stdout, stderr } = await _exec(command, {
+      cwd: workDir,
+      ...options,
+    })
+    if (stderr) {
+      spinner.fail(stderr)
+      process.exit(1)
+    }
+    spinner.text += ` (+${ms(Date.now() - timestamp)})`
+    spinner.succeed()
+    return stdout
+  }
 
   switch (platform) {
     case "win": {
       const HEADER_PATH = path.join(workDir, `${filename}.h`)
       const DEF_PATH = path.join(outDir, `${filename}.def`)
+
+      // find vs tools
+      spinner.start('find vs tools')
       const [VS_TOOLS_PATH] = await glob('**/bin/Hostx64/x64', {
         cwd: "C:/Program Files (x86)/Microsoft Visual Studio",
         onlyDirectories: true,
         absolute: true,
       })
-      if (!VS_TOOLS_PATH) throw new Error('VS_TOOLS_PATH not found')
-      const VS_TOOLS_PATH_ENV = {
-        ...process.env,
-        Path: VS_TOOLS_PATH + path.delimiter + process.env.Path
-      }
+      if (!VS_TOOLS_PATH) throw new Error('vs tools not found')
+      spinner.succeed(`vs tools found at: ${VS_TOOLS_PATH}`)
+      process.env.PATH = VS_TOOLS_PATH + path.delimiter + process.env.PATH
 
-      // Step 1
-      const goResult = await exec(`go build -buildmode c-shared -o ${filename}.dll`, {
-        cwd: workDir
-      })
-      assert.equal(goResult.stderr, "")
-      
-      // Step 2
-      const header = fs.readFileSync(HEADER_PATH, "utf8")
+      // build source code
+      await exec(`go build -buildmode c-shared -o ${filename}.dll`)
+  
+      // patch cgo header
+      spinner.start('patch cgo header')
+      const header = await fs.promises.readFile(HEADER_PATH, "utf8")
       const editedHeader = header.replace("__SIZE_TYPE__", "size_t").replace(/^.* _Complex .*$/gm, "// $&")
-      fs.writeFileSync(HEADER_PATH, editedHeader, "utf8")
+      await fs.promises.writeFile(HEADER_PATH, editedHeader, "utf8")
+      spinner.succeed()
 
-      // Step 3
+      // generate lib from dll
       const exports = await exec(`dumpbin /exports ${filename}.dll`, {
-        env: VS_TOOLS_PATH_ENV,
         maxBuffer: 10 * 1024 * 1024,
-        cwd: workDir
       })
-      assert.equal(exports.stderr, "")
+
       const defFile = [`LIBRARY ${filename}`, "EXPORTS"]
-      const exportsDef = exports.stdout
+      const exportsDef = exports
         .split("\r\n")
         .slice(19) // starts from 20th line
         .flatMap(line => {
@@ -56,11 +71,8 @@ async function build(workDir: string, filename: string, platform: "win") {
           }
         })
       defFile.push(...exportsDef)
-      fs.writeFileSync(DEF_PATH, defFile.join("\r\n"), "utf8")
-      await exec(`lib /def:${filename}.def /out:${filename}.lib /machine:X64`, {
-        env: VS_TOOLS_PATH_ENV,
-        cwd: workDir
-      })
+      await fs.promises.writeFile(DEF_PATH, defFile.join("\r\n"), "utf8")
+      await exec(`lib /def:${filename}.def /out:${filename}.lib /machine:X64`)
       break
     }
 
