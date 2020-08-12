ddd86b69f369af62f814429695d34f75a1cb38dd
Shigma <1700011071@pku.edu.cn>
2020-08-12 14:52:09 +0800

    add final build steps

--- README.md
+++ README.md
@@ -3,30 +3,7 @@
 アトリは、高性能ですから!
 
 ```bat
-REM Step 0: init vs env
-%comspec% /k "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
-
-REM "Step 1: cgo"
-go build -buildmode c-shared -o request.dll request.go
-
-REM "Step 2: patch cgo header"
-REM "Comment out invalid lines in request.h"
-
-REM "Step 3: generate lib from dll"
-dumpbin /exports request.dll > exports.txt
-echo LIBRARY request > request.def
-echo EXPORTS >> request.def
-for /f "skip=19 tokens=4" %%A in (exports.txt) do echo %%A >> request.def
-lib /def:request.def /out:request.lib /machine:X64
-
-REM "Step 4: node-gyp"
-node-gyp configure
-node-gyp build
-
-REM "Step 5: copy dependency"
-copy request.dll build\Release
-
-REM "Step 6: test it out"
+ts-node tools/build
 node ./test.js
 ```
 
--- tools/build.ts
+++ tools/build.ts
@@ -3,61 +3,73 @@ import * as path from "path"
 import * as util from "util"
 import * as fs from "fs"
 import glob from "fast-glob"
+import chalk from "chalk"
 import ora from "ora"
 import ms from "ms"
 
-async function build(workDir: string, filename: string, platform: "win") {
-  const outDir = workDir // path.join(workDir, "build"); mkdirSync(outDir)
+async function build(entry: string, bindingDir: string, platform: "win") {
+  const entryDir = path.dirname(entry)
+  const filename = path.basename(entry)
   const spinner = ora()
+
+  let timestamp: number
+  function markStart(text: string) {
+    timestamp = Date.now()
+    spinner.start(text)
+  }
+
+  function markSucceed() {
+    spinner.text += chalk.green` (+${ms(Date.now() - timestamp)})`
+    spinner.succeed()
+  }
 
   const _exec = util.promisify(cp.exec)
   async function exec (command: string, options: cp.ExecOptions = {}) {
-    const timestamp = Date.now()
-    spinner.start(command)
+    markStart(command)
     const { stdout, stderr } = await _exec(command, {
-      cwd: workDir,
+      cwd: entryDir,
       ...options,
     })
     if (stderr) {
       spinner.fail(stderr)
       process.exit(1)
     }
-    spinner.text += ` (+${ms(Date.now() - timestamp)})`
-    spinner.succeed()
+    markSucceed()
     return stdout
   }
 
   switch (platform) {
     case "win": {
-      const HEADER_PATH = path.join(workDir, `${filename}.h`)
-      const DEF_PATH = path.join(outDir, `${filename}.def`)
+      const HEADER_PATH = path.join(entryDir, `${filename}.h`)
+      const DEF_PATH = path.join(entryDir, `${filename}.def`)
 
       // find vs tools
-      spinner.start('find vs tools')
+      markStart('find vs tools')
       const [VS_TOOLS_PATH] = await glob('**/bin/Hostx64/x64', {
         cwd: "C:/Program Files (x86)/Microsoft Visual Studio",
         onlyDirectories: true,
         absolute: true,
       })
       if (!VS_TOOLS_PATH) throw new Error('vs tools not found')
-      spinner.succeed(`vs tools found at: ${VS_TOOLS_PATH}`)
       process.env.PATH = VS_TOOLS_PATH + path.delimiter + process.env.PATH
+      markSucceed()
 
       // build source code
       await exec(`go build -buildmode c-shared -o ${filename}.dll`)
   
       // patch cgo header
-      spinner.start('patch cgo header')
+      markStart('patch cgo header')
       const header = await fs.promises.readFile(HEADER_PATH, "utf8")
       const editedHeader = header.replace("__SIZE_TYPE__", "size_t").replace(/^.* _Complex .*$/gm, "// $&")
       await fs.promises.writeFile(HEADER_PATH, editedHeader, "utf8")
-      spinner.succeed()
+      markSucceed()
 
       // generate lib from dll
       const exports = await exec(`dumpbin /exports ${filename}.dll`, {
         maxBuffer: 10 * 1024 * 1024,
       })
 
+      markStart('write def file')
       const defFile = [`LIBRARY ${filename}`, "EXPORTS"]
       const exportsDef = exports
         .split("\r\n")
@@ -72,7 +84,21 @@ async function build(workDir: string, filename: string, platform: "win") {
         })
       defFile.push(...exportsDef)
       await fs.promises.writeFile(DEF_PATH, defFile.join("\r\n"), "utf8")
+      markSucceed()
+
       await exec(`lib /def:${filename}.def /out:${filename}.lib /machine:X64`)
+
+      // node-gyp
+      // rebuild = clean + configure + build
+      markStart('node gyp rebuild')
+      await _exec('node-gyp rebuild', { cwd: bindingDir })
+      markSucceed()
+    
+      // copy dependency
+      markStart('copy dependency')
+      await fs.promises.copyFile(entry + ".dll", path.join(bindingDir, 'build/release', filename + '.dll'))
+      markSucceed()
+
       break
     }
 
@@ -81,4 +107,4 @@ async function build(workDir: string, filename: string, platform: "win") {
   }
 }
 
-build("./sample/go", "request", "win")
+build("./sample/go/request", "./sample/binding", "win")
